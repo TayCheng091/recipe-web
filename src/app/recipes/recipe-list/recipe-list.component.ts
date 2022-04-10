407abd6c671af93e13a29fdba39c825e1439e1bd
Tay Cheng <b1233967@gmail.com>
2022-04-10 12:41:50 +0800

    create project

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+
+  public recipes:Recipe[] = [
+    new Recipe("Chocolate","Good","https://picsum.photos/300/300?random=1"),
+    new Recipe("Chocolate","Good","https://picsum.photos/300/300?random=2"),
+    new Recipe("Chocolate","Good","https://picsum.photos/300/300?random=3")
+  ] ;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("recipes = ",this.recipes) ;
+  }
+
+}
